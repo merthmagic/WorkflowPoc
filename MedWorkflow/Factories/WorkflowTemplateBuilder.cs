4d9a8e884816098fbeb242157eef95d986736f80
yimlu <luyiming2009@gmail.com>
2017-02-23 12:44:10 +0800

    add missed file

---
+++ MedWorkflow/Factories/WorkflowTemplateBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MedWorkflow.Factories
+{
+    internal class WorkflowTemplateBuilder
+    {
+        private WorkflowTemplate _workflowTemplate;
+        private IEnumerable<ActivityTemplate> _activityTemplates;
+ 
+        class TransitPath
+        {
+            public ActivityTemplate FromActivityTemplate { get; set; }
+            public ActivityTemplate ToActivityTemplate { get; set; }
+        }
+
+        public WorkflowTemplateBuilder SetWorkflowTemplate(WorkflowTemplate workflowTemplate)
+        {
+            _workflowTemplate = workflowTemplate;
+            return this;
+        }
+
+        public WorkflowTemplateBuilder SetActivityTemplates(IEnumerable<ActivityTemplate> activityTemplates)
+        {
+            _activityTemplates = activityTemplates;
+            return this;
+        }
+
+        public IWorkflowTemplate Build()
+        {
+            var dict = new Dictionary<int, ActivityTemplate>();
+
+            //设置所有Activity对Workflow的引用
+            foreach (var activityTemplate in _activityTemplates)
+            {
+                activityTemplate.WorkflowTemplate = _workflowTemplate;
+
+                dict.Add(activityTemplate.ActivityTemplateId,activityTemplate);
+            }
+
+            //构建流转路径
+            foreach (var activity in _activityTemplates)
+            {
+                foreach (var action in activity.Actions)
+                {
+                    if (action.TargetActivityId > 0)
+                        action.Transit = dict[action.TargetActivityId];
+                }
+            }
+
+            _workflowTemplate.AddActivityTemplates(_activityTemplates);
+            
+            return _workflowTemplate;
+        }
+    }
+}
